910a43b94c94a0848dac7437c52d5dc3633df026
Kacper Agatowski <n00212272@iadt.ie>
2023-12-18 12:13:10 +0000

    Changed nav

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -9,7 +9,6 @@ const MyNavBar = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-neutral text-white rounded-box w-52">
-              <li><Link to='/'>Home</Link></li>
               <li><Link to='/about'>About</Link></li>
               <li><Link to='/projects/home'>Projects</Link></li>
               <li><Link to='/contact'>Contact</Link></li>
@@ -17,7 +16,7 @@ const MyNavBar = () => {
           </div>
         </div>
         <div className="navbar-center">
-          <a href className="btn btn-ghost normal-case text-xl text-white">Kacpers Porfolio</a>
+        <Link to='/'><a href className="btn btn-ghost normal-case text-xl text-white">Kacpers Porfolio</a></Link>
         </div>
         <div className="navbar-end">
           
