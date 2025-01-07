cde0e0c35e2f0fd21e8ccfe824083d543daec443
Kacper Agatowski <n00212272@iadt.ie>
2025-01-07 14:31:17 +0000

    style

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -6,7 +6,7 @@ const MyNavBar = ({term,handleChange}) => {
       handleChange(e);
   };
     return (
-        <div className="navbar bg-primary">
+        <div className="navbar">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost bg-base-200 btn-circle">
