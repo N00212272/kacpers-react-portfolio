322afbcadd85363898f710a37acb2c6661cbd739
Kacper Agatowski <n00212272@iadt.ie>
2025-01-07 14:33:52 +0000

    navbar

--- src/components/MyNavBar.js
+++ src/components/MyNavBar.js
@@ -6,13 +6,13 @@ const MyNavBar = ({term,handleChange}) => {
       handleChange(e);
   };
     return (
-        <div className="navbar">
+        <div className="navbar w-full">
         <div className="navbar-start">
           <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost bg-base-200 btn-circle">
+            <label tabIndex={0} className="btn btn-ghost bg-transparent btn-circle">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
             </label>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-neutral text-white rounded-box w-52">
+            <ul tabIndex={0} className="menu menu-sm dropdown-content p-2 shadow bg-neutral text-white rounded-box w-52">
               <li><Link to='/about'>About</Link></li>
               <li><Link to='/projects/home'>Projects</Link></li>
               <li><Link to='/contact'>Contact</Link></li>
